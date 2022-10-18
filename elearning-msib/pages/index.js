3d7f70c1e82254875a99ecd8231180afb37962ef
VenziUno <pendicai3@gmail.com>
2022-10-18 16:11:04 +0700

    main

--- elearning-msib/pages/index.js
+++ elearning-msib/pages/index.js
@@ -1,7 +1,9 @@
+import Buttons from "../Components/Buttons";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Buttons/>
+    </>
   )
 }

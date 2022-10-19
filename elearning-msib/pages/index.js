08ddd6c17b773c044273a5451bd2dda8661b8401
VenziUno <pendicai3@gmail.com>
2022-10-19 14:21:22 +0700

    main

--- elearning-msib/pages/index.js
+++ elearning-msib/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../Components/Layout";
 export default function Home() {
   return (
     <>
-      <Buttons classColor="btn-primary">Primary</Buttons>
+      {/* <Buttons classColor="btn-primary">Primary</Buttons>
       <Buttons classColor="btn-secondary">Secondary</Buttons>
       <Buttons classColor="btn-light">Light</Buttons>
       <Buttons classColor="btn-dark">Dark</Buttons>
@@ -12,7 +12,7 @@ export default function Home() {
       <Buttons classColor="btn-success">Success</Buttons>
       <Buttons classColor="btn-warning">Warning</Buttons>
       <Buttons classColor="btn-danger">Danger</Buttons>
-      <Buttons classColor="btn-link">Print</Buttons>
+      <Buttons classColor="btn-link">Print</Buttons> */}
 
       <Layout>
         <div className="flex flex-col gap-4 text-sm text-justify">

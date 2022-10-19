d77438ac54a724374116dcc6e90d37a1edead0ca
VenziUno <pendicai3@gmail.com>
2022-10-19 15:41:36 +0700

    pendi - main

--- elearning-msib/pages/index.js
+++ elearning-msib/pages/index.js
@@ -35,6 +35,17 @@ export default function Home() {
           </p>
         </div>
       </Layout>
+      <div className='m-5'>
+            <Buttons classColor="btn-primary">Primary</Buttons>
+            <Buttons classColor="btn-secondary">Secondary</Buttons>
+            <Buttons classColor="btn-light">Light</Buttons>
+            <Buttons classColor="btn-dark">Dark</Buttons>
+            <Buttons classColor="btn-info">Info</Buttons>
+            <Buttons classColor="btn-success">Success</Buttons>
+            <Buttons classColor="btn-warning">Warning</Buttons>
+            <Buttons classColor="btn-danger">Danger</Buttons>
+            <Buttons classColor="btn-link">Print</Buttons>
+        </div>
     </>
   );
 }

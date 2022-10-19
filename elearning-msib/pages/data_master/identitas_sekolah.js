1cfc8cfd5f68e8c5d0d2659cd02e5527851efcba
shirleen-sho <shirleen.sho@gmail.com>
2022-10-19 14:13:39 +0700

    shirleen - add testing page (home & info sekolah)

---
+++ elearning-msib/pages/data_master/identitas_sekolah.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../../Components/Layout";
+
+const IdentitasSekolah = () => {
+  return (
+    <Layout>
+      <div>
+        <p>content here</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default IdentitasSekolah;
--- elearning-msib/pages/index.js
+++ elearning-msib/pages/index.js
@@ -4,24 +4,35 @@ import Layout from "../Components/Layout";
 export default function Home() {
   return (
     <>
-      <Buttons>Pendi</Buttons>
       <Layout>
-        <div className="flex flex-col flex-grow">
-          <div className="flex flex-row justify-between items-center py-5 border-b border-neutral-2">
-            <div className="flex flex-col gap-2">
-              <div className="font-bold text-3xl">Home</div>
-              <div className="font-medium text-sm">Home details here!</div>
-            </div>
-            <div className="flex flex-row items-center gap-5 bg-warning-base">
-              <div className="bg-white p-4 rounded-full">icon1</div>
-              <div className="bg-white p-4 rounded-full">icon2</div>
-              <div className="flex flex-col gap-1 items-center">
-                <div className="font-semibold text-sm">Shirleen</div>
-                <div className="font-normal text-sm">Admin</div>
-              </div>
-            </div>
-          </div>
-          <div className="bg-primary-50 h-full py-5">content</div>
+        <div className="flex flex-col gap-4 text-sm text-justify">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            tincidunt nulla velit. Praesent aliquet massa at odio dictum, sed
+            imperdiet elit volutpat. Phasellus in ante semper, ornare justo
+            quis, auctor nunc. Fusce non ipsum sed purus congue posuere non nec
+            libero. Sed fringilla varius urna sit amet aliquet. Etiam eleifend,
+            magna eget porta tempor, ante arcu gravida metus, et ultrices leo
+            turpis posuere purus. Cras ligula quam, consequat vitae nunc vitae,
+            egestas imperdiet risus. Sed porta, lectus ut tincidunt luctus, dui
+            sem iaculis erat, facilisis molestie enim erat non nisl. Vestibulum
+            et purus felis. Sed sodales, ante vitae malesuada rutrum, tellus
+            tellus viverra nunc, facilisis elementum ligula ex eu elit. Vivamus
+            quis arcu vel libero mattis varius quis eu est. Nam ut risus cursus,
+            pharetra dolor ut, efficitur ex. Proin tristique scelerisque elit,
+            ac ultricies est ultricies vitae. Proin volutpat quam et iaculis
+            elementum.
+          </p>
+          <p>
+            In elementum diam non ipsum mollis, at volutpat nisl ullamcorper.
+            Proin volutpat volutpat velit ac lobortis. Nam malesuada dignissim
+            dignissim. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Quisque malesuada ac arcu sit amet euismod. Nam sed
+            suscipit nibh, et aliquet diam. Praesent pretium malesuada dolor et
+            porttitor. Morbi quis libero sapien. Ut vel nisi dolor. Class aptent
+            taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos.
+          </p>
         </div>
       </Layout>
     </>

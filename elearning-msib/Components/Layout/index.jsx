0aacc1395cf9c73444f673cdf2d4a2d7c66b7164
shirleen-sho <shirleen.sho@gmail.com>
2022-10-19 14:10:19 +0700

    shirleen - edit layout & add header

--- elearning-msib/Components/Layout/index.jsx
+++ elearning-msib/Components/Layout/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SideMenu from "./SideMenu";
+import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-row gap-10 p-10">
       <SideMenu />
-      {children}
+      <div className="flex flex-col flex-grow h-full">
+        <Header />
+        <div className="h-full py-5">{children}</div>
+      </div>
     </div>
   );
 };

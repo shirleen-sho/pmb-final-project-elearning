34dab9be2727002a9c1d2cbe5f5102a71fd3bcab
VenziUno <pendicai3@gmail.com>
2022-10-20 12:02:05 +0700

    Pendi - Input

--- elearning-msib/Components/TextArea/index.jsx
+++ elearning-msib/Components/TextArea/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
-const TextArea = () => {
+const TextArea = ({placeholder}) => {
+  const[value , setValue] = useState('')
+  const handleValue= (e) => {
+    setValue(e.target.value)
+  }
   return (
     <>
-      <textarea className='w-full border shadow bg-white m-2 h-fit px-2 py-2 rounded outline-none'/>
+      <textarea value={value} onChange={handleValue} placeholder={placeholder} className='w-full border shadow bg-white m-2 h-fit px-2 py-2 rounded outline-none'/>
     </>
   )
 }

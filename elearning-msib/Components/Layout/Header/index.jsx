7f74d809da4fb76af2206799f85aa5d9498c00e3
shirleen-sho <shirleen.sho@gmail.com>
2022-10-21 09:23:36 +0700

    shirleen - fix header : add link

--- elearning-msib/Components/Layout/Header/index.jsx
+++ elearning-msib/Components/Layout/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { useAppContext } from "../../../Hooks/useAppContext";
 
 const Header = () => {
@@ -33,50 +34,70 @@ const Header = () => {
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
               />
             </svg>
-            <span class="bg-primary-400 h-2 w-2 rounded-full absolute -right-1 -top-1"></span>
+            <span className="bg-primary-400 h-2 w-2 rounded-full absolute -right-1 -top-1"></span>
           </div>
         </div>
 
-        <div
-          id="icon-settings"
-          className="bg-white p-4 rounded-full text-neutral-3 hover:cursor-pointer hover:text-primary-400"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="w-6 h-6"
+        <Link href="/settings">
+          <a
+            id="icon-settings"
+            className="bg-white p-4 rounded-full text-neutral-3 hover:cursor-pointer hover:text-primary-400"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-        </div>
-        <div className="flex flex-col gap-1 justify-center items-end px-4">
-          <div className="font-semibold text-sm text-neutral-4">
-            {user_info.name}
-          </div>
-          <div className="font-normal text-sm text-neutral-3">
-            {user_info.role}
-          </div>
-        </div>
-        <div id="profile_pic" className="bg-primary-300 w-14 h-14 rounded-full">
-          {user_info.photo && (
-            <img
-              src={`/images/${user_info.photo}`}
-              className="object-cover h-full w-auto rounded-full"
-            />
-          )}
-        </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </a>
+        </Link>
+        <Link href="/profile">
+          <a className="flex flex-row items-center gap-4">
+            <div className="flex flex-col gap-1 justify-center items-end px-4">
+              <div className="font-semibold text-sm text-neutral-4">
+                {user_info.name}
+              </div>
+              <div className="font-normal text-sm text-neutral-3">
+                {user_info.role}
+              </div>
+            </div>
+            {user_info.photo ? (
+              <img
+                src={`/images/${user_info.photo}`}
+                className="object-cover h-14 w-14 rounded-full"
+              />
+            ) : (
+              // image placeholder
+              <div className="overflow-hidden relative w-14 h-14 rounded-full bg-gray-100 dark:bg-gray-600">
+                <svg
+                  className="absolute -left-1 w-16 h-16 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            )}
+          </a>
+        </Link>
       </div>
     </div>
   );

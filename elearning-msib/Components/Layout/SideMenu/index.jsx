fe2be883eb3dd439e6dc832ecc2f48a118b27876
shirleen-sho <shirleen.sho@gmail.com>
2022-10-22 08:48:05 +0700

    shirleen - add function show hide menu

--- elearning-msib/Components/Layout/SideMenu/index.jsx
+++ elearning-msib/Components/Layout/SideMenu/index.jsx
@@ -4,6 +4,17 @@ import { useRouter } from "next/router";
 import { useAppContext } from "../../../Hooks/useAppContext";
 
 const SideMenu = () => {
+  const [showSidemenu, setShowSidemenu] = useState(true);
+  const [paddingMenu, setPaddingMenu] = useState("px-6 py-2");
+  const handleToggleSidemenu = () => {
+    setShowSidemenu(!showSidemenu);
+    if (showSidemenu) {
+      setPaddingMenu("px-3 py-2");
+    } else {
+      setPaddingMenu("px-6 py-2");
+    }
+  };
+
   const { menu } = useAppContext();
   const {
     menus,
@@ -36,11 +47,21 @@ const SideMenu = () => {
   }, []);
 
   return (
-    <div className="bg-primary-100 flex flex-col gap-10 px-2 py-14 rounded-3xl min-h-screen h-fit min-w-fit">
-      <div className="flex flex-row justify-center">
-        <img src="/images/logo.png" className="h-14 w-auto" alt="icon school" />
+    <div className="relative bg-primary-100 flex flex-col gap-10 px-2 py-14 rounded-3xl min-h-screen h-fit min-w-fit">
+      <div className="flex flex-row justify-center h-14">
+        <img
+          src={showSidemenu ? "/images/logo.png" : "/images/icon/school.png"}
+          className={showSidemenu ? "h-full" : "h-7"}
+          alt="icon school"
+        />
+        <button
+          onClick={handleToggleSidemenu}
+          className="absolute -top-2 -right-2 bg-primary-50 shadow-sm w-7 h-7 rounded-full"
+        >
+          {showSidemenu ? "<" : ">"}
+        </button>
       </div>
-      <div>
+      <div className="flex flex-col gap-1">
         {menus.map(({ menu, sub_menu }) => (
           <div className="flex flex-col gap-1" key={menu.route}>
             <Link
@@ -50,8 +71,8 @@ const SideMenu = () => {
               <a
                 className={
                   selectedMenu === menu.route
-                    ? "font-semibold text-xs px-6 py-2 bg-primary-200 rounded-full"
-                    : "font-semibold text-xs px-6 py-2 hover:bg-primary-200 hover:rounded-full"
+                    ? `font-semibold text-xs ${paddingMenu} bg-primary-200 rounded-full`
+                    : `font-semibold text-xs ${paddingMenu} hover:bg-primary-200 hover:rounded-full`
                 }
                 onClick={() => {
                   if (sub_menu) {
@@ -60,6 +81,9 @@ const SideMenu = () => {
                     setSelectedSubmenu("");
                   }
                   setSelectedMenu(menu.route);
+                  if (!showSidemenu) {
+                    setShowSidemenu(true);
+                  }
                 }}
               >
                 <div className="flex flex-row justify-between items-center gap-4">
@@ -73,11 +97,11 @@ const SideMenu = () => {
                       alt="icon"
                       className="w-4 h-auto"
                     />
-                    {menu.name}
+                    {showSidemenu && menu.name}
                   </div>
                   {
                     /* icon dropdown */
-                    sub_menu && (
+                    showSidemenu && sub_menu && (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -97,7 +121,7 @@ const SideMenu = () => {
                 </div>
               </a>
             </Link>
-            {selectedMenu === menu.route && sub_menu && (
+            {showSidemenu && selectedMenu === menu.route && sub_menu && (
               <div className="flex flex-row justify-center">
                 <div className="bg-primary-50 flex flex-col w-4/5">
                   {sub_menu.length > 0 &&

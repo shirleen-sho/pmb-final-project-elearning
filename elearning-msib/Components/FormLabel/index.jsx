0908182e4c562dc2f350cbdcab8e5c416b2cb1d3
shirleen-sho <shirleen.sho@gmail.com>
2022-10-27 11:35:54 +0700

    shirleen - update FormLabel & FormBanner

--- elearning-msib/Components/FormLabel/index.jsx
+++ elearning-msib/Components/FormLabel/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 const FormLabel = ({ rowSpan, className, children }) => {
-  if (rowSpan === "" || rowSpan === undefined) {
-    rowSpan = 1; // row-span-${rowSpan}
-  }
   return (
     <div className={`font-semibold text-[13px] leading-5 ${className}`}>
       {children}

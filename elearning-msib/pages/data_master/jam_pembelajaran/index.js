47b4f2ec06b2102de66eb00a2d4cea41c1ac63d5
shirleen-sho <shirleen.sho@gmail.com>
2022-10-28 11:53:41 +0700

    shirleen - add content page Tingkatan & Jam

---
+++ elearning-msib/pages/data_master/jam_pembelajaran/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "../../../Components/Layout";
+import Table from "../../../Components/Table";
+import { useAppContext } from "../../../Hooks/useAppContext";
+
+const JamPembelajaran = () => {
+  const { dummy } = useAppContext();
+  const { list_jam_pembelajaran } = dummy;
+  return (
+    <Layout>
+      <div className="w-full h-full">
+        <Table
+          data={list_jam_pembelajaran}
+          textAlign="center"
+          action="archive-edit"
+          topNavigation={true}
+          tableTitle="Daftar Jam Pembelajaran"
+          buttonAddLink="/data_master/jam_pembelajaran/add"
+          buttonAddText="Add Jam Pembelajaran"
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default JamPembelajaran;
---
+++ elearning-msib/pages/data_master/tingkatan/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "../../../Components/Layout";
+import Table from "../../../Components/Table";
+import { useAppContext } from "../../../Hooks/useAppContext";
+
+const Tingkatan = () => {
+  const { dummy } = useAppContext();
+  const { list_tingkatan } = dummy;
+  return (
+    <Layout>
+      <div className="w-full h-full">
+        <Table
+          data={list_tingkatan}
+          textAlign="center"
+          action="archive-edit"
+          topNavigation={true}
+          tableTitle="Daftar Tingkatan"
+          buttonAddLink="/data_master/tingkatan/add"
+          buttonAddText="Add Tingkatan"
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default Tingkatan;

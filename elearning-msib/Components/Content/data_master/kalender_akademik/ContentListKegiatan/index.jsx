519c20c99f63f9f843a42e8b3cbe9747e1c39719
shirleen-sho <shirleen.sho@gmail.com>
2022-10-29 08:42:21 +0700

    shirleen - update some content page kalender style

--- elearning-msib/Components/Content/data_master/kalender_akademik/ContentListKegiatan/index.jsx
+++ elearning-msib/Components/Content/data_master/kalender_akademik/ContentListKegiatan/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAppContext } from "../../../../../Hooks/useAppContext";
 import Buttons from "../../../../Buttons";
+import InputFields from "../../../../InputFields";
 import Table from "../../../../Table";
 
 const ContentListKegiatan = () => {
@@ -8,10 +9,10 @@ const ContentListKegiatan = () => {
   const { list_kegiatan } = dummy;
   return (
     <div className="w-full h-full py-3">
-      <div className="flex flex-row justify-center gap-3 absolute top-0 right-0">
-        <Buttons type="secondary">O</Buttons>
+      <div className="flex flex-row justify-center items-center gap-3 absolute top-0 right-0">
+        <InputFields type="text" placeholder="Search" />
         <Buttons type="primary" link="/data_master/kalender_akademik/add">
-          Add Kegiatan
+          Add
         </Buttons>
       </div>
       <Table data={list_kegiatan} action="archive-edit" />

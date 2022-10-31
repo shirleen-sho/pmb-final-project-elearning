47eb3225bc4de13ae559667f6120859a6060bf8a
shirleen-sho <shirleen.sho@gmail.com>
2022-10-31 16:39:27 +0700

    shirleen - update Link

--- elearning-msib/Components/Table/index.jsx
+++ elearning-msib/Components/Table/index.jsx
@@ -113,67 +113,72 @@ const Table = ({ data, action, tableTitle, topNavigation, buttonAddLink }) => {
         </span>
         {/* pagination */}
         <div className="flex flex-row items-center gap-4 text-xs">
-          <Link href="/">
-            <a className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </a>
-          </Link>
-          <Link href="/">
-            <a
-              aria-current="page"
-              className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-primary-300 border border-primary-300 text-white"
+          <Link
+            href="/"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100"
+          >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              1
-            </a>
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </Link>
-          <Link href="/">
-            <a className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100">
-              2
-            </a>
+          <Link
+            href="/"
+            aria-current="page"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-primary-300 border border-primary-300 text-white"
+          >
+            1
           </Link>
-          <Link href="/">
-            <a className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100">
-              3
-            </a>
+          <Link
+            href="/"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100"
+          >
+            2
           </Link>
-          <Link href="/">
-            <a className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100">
-              ...
-            </a>
+          <Link
+            href="/"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100"
+          >
+            3
           </Link>
-          <Link href="/">
-            <a className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100">
-              100
-            </a>
+          <Link
+            href="/"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100"
+          >
+            ...
           </Link>
-          <Link href="/">
-            <a className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </a>
+          <Link
+            href="/"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100"
+          >
+            100
+          </Link>
+          <Link
+            href="/"
+            className="font-medium px-1.5 py-1.5 w-8 flex justify-center items-center rounded bg-gray-100 border border-gray-200 hover:bg-primary-100"
+          >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </Link>
         </div>
       </div>

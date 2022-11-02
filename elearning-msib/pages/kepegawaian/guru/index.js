0b37e53078fa811c1678551a888337c2ba7d5369
shirleen-sho <shirleen.sho@gmail.com>
2022-11-02 13:35:35 +0700

    shirleen - create folder kepegawaian

---
+++ elearning-msib/pages/kepegawaian/guru/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../../Components/Layout";
+
+const Guru = () => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  );
+};
+
+export default Guru;
---
+++ elearning-msib/pages/kepegawaian/staff/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../../Components/Layout";
+
+const Staff = () => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  );
+};
+
+export default Staff;

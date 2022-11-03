294b4a8dc9af902df12d8f4b14d84a42866cb85f
VenziUno <pendicai3@gmail.com>
2022-11-03 11:40:23 +0700

    main

--- Components/Card/index.jsx
+++ Components/Card/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { FaInstagram, FaPhone, FaTwitter } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { HiOutlineDotsVertical } from "react-icons/hi";
-import Buttons from "../Buttons";
+import Button from "../Buttons";
 
 export default function Card({ users }) {
   return (
@@ -15,9 +15,11 @@ export default function Card({ users }) {
           className="flex flex-col justify-center border w-52 h-64 shadow rounded-xl m-2 hover:border-blue-500 hover:border-2"
           key={index}
         >
-          <button className="flex justify-end mx-3">
-            <HiOutlineDotsVertical/>
-          </button>
+          <div className="flex justify-end ">
+            <Button variant="icon" type="icon">
+              <HiOutlineDotsVertical/>
+            </Button>
+          </div>
           <Image
             src={value.image}
             alt={value.desc}
@@ -25,13 +27,12 @@ export default function Card({ users }) {
             width={1000}
             height={1000}
           />
-          
           <div className="space-y-2 text-center divide-y ">
             <div>
               <div className="text-2xl font-bold leading-8 tracking-widest">{value.name}</div>
               <div className="font-light text-xs text-gray-400">{value.nuptk}</div>
             </div>
-            <div className="flex justify-center space-x-4 space-y pt-3 divide-y  ">
+            <div className="flex justify-center space-x-4 space-y pt-2 divide-y  ">
               <FaPhone/>
               <MdEmail/>
               <FaInstagram/>

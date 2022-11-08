4d388a3fc7004c7771038bec401be7ddb1376562
shirleen-sho <shirleen.sho@gmail.com>
2022-11-08 14:28:46 +0700

    shirleen - update content AddRPP & AddSilabus

--- Components/FormItem/index.jsx
+++ Components/FormItem/index.jsx
@@ -1,28 +1,38 @@
 import React from "react";
 
-const FormItem = ({ label, labelType, labelWidth, children }) => {
+const FormItem = ({ label, labelType, labelWidth, helper, children }) => {
+  let labelStyle;
   switch (labelType) {
     case "banner":
-      labelType =
+      labelStyle =
         "font-semibold text-lg bg-primary-100 rounded-md px-5 py-3 w-full";
       break;
     case "label-sm":
-      labelType = "font-semibold text-xs leading-5";
+      labelStyle = "font-semibold text-xs leading-5";
       break;
     case "label-md":
-      labelType = "font-bold text-base";
+      labelStyle = "font-bold text-base";
       break;
     case "read-only":
-      labelType = "text-xs bg-gray-100 rounded-md px-5 py-3 w-full";
+      labelStyle = "text-xs bg-gray-100 rounded-md px-5 py-3 w-full";
       break;
     default:
-      labelType = "";
+      labelStyle = "";
       break;
   }
 
   return (
     <div className={`flex flex-row gap-5`}>
-      <div className={`${labelType} ${labelWidth}`}>{label}</div>
+      {helper ? (
+        <div className={`flex flex-col gap-1 ${labelWidth}`}>
+          <div className={`${labelStyle}`}>{label}</div>
+          <div className="text-[8px] italic ">
+            File must be in PDF & docs only
+          </div>
+        </div>
+      ) : (
+        <div className={`${labelStyle} ${labelWidth}`}>{label}</div>
+      )}
       {children && <div className="w-full">{children}</div>}
     </div>
   );
--- pages/data_akademik/silabus/index.js
+++ pages/data_akademik/silabus/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "../../../Components/Layout";
+import Table from "../../../Components/Table";
+import { useAppContext } from "../../../Hooks/useAppContext";
 
 const Silabus = () => {
+  const { dummy } = useAppContext();
+  const { list_ruangan } = dummy;
   return (
     <Layout>
-      <div></div>
+      <div className="w-full h-full">
+        <Table
+          data={list_ruangan}
+          action="archive-edit"
+          topNavigation={true}
+          tableTitle="Daftar Silabus"
+        />
+      </div>
     </Layout>
   );
 };

ee61a435d15388ad1be7ab92f338002291033625
shirleen-sho <shirleen.sho@gmail.com>
2022-11-08 11:24:03 +0700

    shirleen - add "read-only" labelType to FormItem

--- Components/FormItem/index.jsx
+++ Components/FormItem/index.jsx
@@ -3,14 +3,17 @@ import React from "react";
 const FormItem = ({ label, labelType, labelWidth, children }) => {
   switch (labelType) {
     case "banner":
-      labelType = "font-semibold text-lg bg-primary-100 rounded-md px-5 py-3 w-full";
-      labelWidth = "";
+      labelType =
+        "font-semibold text-lg bg-primary-100 rounded-md px-5 py-3 w-full";
       break;
     case "label-sm":
       labelType = "font-semibold text-xs leading-5";
       break;
     case "label-md":
       labelType = "font-bold text-base";
+      break;
+    case "read-only":
+      labelType = "text-xs bg-gray-100 rounded-md px-5 py-3 w-full";
       break;
     default:
       labelType = "";

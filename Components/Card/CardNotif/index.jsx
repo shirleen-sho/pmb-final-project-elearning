1e1e23477cf979dd0c3c4d1efb205beb4f5acdc7
shirleen-sho <shirleen.sho@gmail.com>
2022-11-08 21:39:19 +0700

    shirleen - update CardNotif for logout

--- Components/Card/CardNotif/index.jsx
+++ Components/Card/CardNotif/index.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Image from "next/image";
 import Button from "../../Buttons";
 import { AiOutlineClose } from "react-icons/ai";
+import { useAppContext } from "../../../Hooks/useAppContext";
 
 export default function CardNotif({ type, desc, title }) {
+  const { user } = useAppContext();
+  const { setShowLogout } = user;
+
   return (
-    <div className="relative flex justify-center align-middle items-center w-full h-screen ">
-      <div className="w-96 py-4 border shadow flex flex-col items-center justify-center rounded-xl space-y-3 m-2">
+    <div className="absolute z-50 backdrop-blur-sm bg-white/30 flex justify-center align-middle items-center w-full h-screen ">
+      <div className="bg-white w-96 py-4 border shadow flex flex-col items-center justify-center rounded-xl space-y-3 m-2">
         {type ? null : (
           <div className="w-full flex justify-end px-5">
             <Button type="icon">
@@ -37,8 +41,16 @@ export default function CardNotif({ type, desc, title }) {
         <div className="w-full flex justify-center items-center px-10">
           {type ? (
             <div className="w-full flex justify-evenly">
-              <Button type="light">Delete</Button>
-              <Button>Cancel</Button>
+              <Button type="light">
+                {type === "logout" ? "Logout" : "Delete"}
+              </Button>
+              <Button
+                handleClick={
+                  type === "logout" ? () => setShowLogout(false) : ""
+                }
+              >
+                Cancel
+              </Button>
             </div>
           ) : null}
         </div>

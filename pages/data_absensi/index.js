355a763b765df62fcefeb8b905248f78f6035c6e
VenziUno <pendicai3@gmail.com>
2022-11-12 12:11:53 +0700

    pendi - main

---
+++ pages/data_absensi/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../Components/Layout'
+
+const Absensi = () => {
+  return (
+    <Layout></Layout>
+  )
+}
+
+export default Absensi
---
+++ pages/log_activity/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Components/Layout'
+
+const LogActivity = () => {
+  return (
+    <>
+    <Layout></Layout>
+    </>
+  )
+}
+
+export default LogActivity

d78a953a5913eb179028f60302c00dcf883e5c45
VenziUno <pendicai3@gmail.com>
2022-11-12 12:01:59 +0700

    pendi - elerning

---
+++ pages/elearning/penyumpulan_tugas/detail/[id].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../../../../Components/Layout"
+
+const DetailPengumpulanTugas = () => {
+  return (
+    <>
+        <Layout>
+            
+        </Layout>
+    </>
+  )
+}
+
+export default DetailPengumpulanTugas

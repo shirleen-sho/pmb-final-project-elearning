4153e46ec507a12a54ff408646ea13871d6e9166
shirleen-sho <shirleen.sho@gmail.com>
2022-11-14 14:00:35 +0700

    shirleen - fix input

--- Components/InputFields/index.jsx
+++ Components/InputFields/index.jsx
@@ -1,44 +1,51 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-const InputFields = ({type, size, placeholder, disabled, readOnly , style, label, valueLock, defaultValue}) => {
-  const[value , setValue] = useState('')
-  const handleValue= (e) => {
-    setValue(e.target.value)
-  }
-  
+const InputFields = ({
+  type,
+  size,
+  placeholder,
+  disabled,
+  readOnly,
+  style,
+  label,
+  valueLock,
+  defaultValue,
+  value,
+  setValue,
+}) => {
+  // const[value , setValue] = useState('')
+
   switch (type) {
     case "date":
-      style='uppercase'
+      style = "uppercase";
       break;
     default:
-      style='capitalize'
+      style = "capitalize";
       break;
   }
 
   switch (disabled) {
     case true:
-      style='bg-gray-200'
-      placeholder = null
+      style = "bg-gray-200";
+      placeholder = null;
       break;
   }
 
   return (
     <>
-        <input 
-        type={type} 
+      <input
+        type={type}
         value={value || valueLock}
-        onChange={handleValue} 
-        className={`border border-gray-200 bg-white shadow rounded px-4 text-xs py-2 outline-none  ${size} ${style}`} 
+        onChange={(e) => setValue(e)}
+        className={`border border-gray-200 bg-white shadow rounded px-4 text-xs py-2 outline-none  ${size} ${style}`}
         placeholder={placeholder}
-        label = {label || placeholder}
+        label={label || placeholder}
         disabled={disabled}
         readOnly={readOnly}
         defaultValue={defaultValue}
-        />
+      />
     </>
-  )
-}
+  );
+};
 
-
-
-export default InputFields
+export default InputFields;

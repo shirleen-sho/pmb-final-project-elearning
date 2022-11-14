af6af2548e25e81673bbc0b417158dd3f318f754
VenziUno <pendicai3@gmail.com>
2022-11-14 09:14:58 +0700

    pendi -richeditor

--- Components/RichEditor/index.js
+++ Components/RichEditor/index.js
@@ -1,33 +1,40 @@
 import React, { Component } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import { EditorState, convertToRaw } from "draft-js";
+import { EditorState } from "draft-js";
+// import { Editor } from "react-draft-wysiwyg";
+import dynamic from "next/dynamic";
 
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+const Editor = dynamic(
+  () => {
+    return import("react-draft-wysiwyg").then(mod => mod.Editor);
+  },
+  { ssr: false }
+);
 
-export default class TextEditor extends Component {
-  state = {
-    editorState: EditorState.createEmpty(),
-  };
+class MyEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+  }
 
-  onEditorStateChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
+  onEditorStateChange = editorState => {
+    this.setState({ editorState });
   };
 
   render() {
     const { editorState } = this.state;
-    
+
     return (
       <div>
         <Editor
           editorState={editorState}
-          toolbarClassName="toolbarClassName"
-          wrapperClassName="wrapperClassName"
-          editorClassName="editorClassName"
+          wrapperClassName="rich-editor demo-wrapper"
+          editorClassName="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
+          placeholder="The message goes here..."
         />
       </div>
     );
   }
 }
+
+export default MyEditor;

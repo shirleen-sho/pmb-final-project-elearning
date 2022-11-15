11ff76a0387d8a21eda3cfd4e7f13e9aea68afb6
VenziUno <pendicai3@gmail.com>
2022-11-15 16:45:03 +0700

    pendi - Detail Tugas dan Nilai

--- pages/pengumuman/add.js
+++ pages/pengumuman/add.js
@@ -1,10 +1,18 @@
 import React from "react";
+import FormItem from "../../Components/FormItem";
 import Layout from "../../Components/Layout";
+import InputFields from "../../Components/InputFields";
+import Selects from "../../Components/Selects";
+import Button from "../../Components/Buttons";
+import RichEditor from "../../Components/RichEditor"
 
 const AddPengumuman = () => {
   return (
     <Layout>
-      <div></div>
+      <div className="flex flex-col gap-5">
+        <FormItem label="Fill Siswa details here!" labelType="banner" />
+        
+      </div>
     </Layout>
   );
 };

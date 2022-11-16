e2eeb0c3542299b8661cb7d6cbdea5cd8900217f
VenziUno <pendicai3@gmail.com>
2022-11-16 16:06:32 +0700

    pendi -almuni

--- pages/Alumni/index.js
+++ pages/Alumni/index.js
@@ -1,28 +1,24 @@
 import React from "react";
-import Layout from "../../../Components/Layout";
-import Button from "../../../Components/Buttons";
-import Search from "../../../Components/Search";
-import CardUsers from "../../../Components/Card/CardUsers";
-import { useAppContext } from "../../../Hooks/useAppContext";
-import Tabs from "../../../Components/Tabs";
-import Selects from "../../../Components/Selects";
+import Layout from "../../Components/Layout";
+import Button from "../../Components/Buttons";
+import Search from "../../Components/Search";
+import CardUsers from "../../Components/Card/CardUsers";
+import { useAppContext } from "../../Hooks/useAppContext";
+import Tabs from "../../Components/Tabs";
+import Selects from "../../Components/Selects";
 
 const Alumni = () => {
   const { dummy } = useAppContext();
   const { users } = dummy;
 
+
   return (
     <Layout>
-      <Tabs list={list} pathName="/kepegawaian">
-        <div className="flex flex-col">
-          {/* container top */}
-          <div className="flex flex-row items-center gap-4 absolute top-0 right-0">
-            <Search></Search>
-            <Button type="warning" link="/kepegawaian/guru/archive">active</Button>
-            <Button link="/kepegawaian/guru/add">add</Button>
-          </div>
-        </div>
-      </Tabs>
+      <div className="flex justify-end gap-4 mb-4">
+        <Search></Search>
+        <Button type="warning" link="/kepegawaian/guru/archive">active</Button>
+        <Button link="/kepegawaian/guru/add">add</Button>
+      </div>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-2 md:gap-4 lg:gap-6 ">
         <CardUsers users={users}></CardUsers>
       </div>

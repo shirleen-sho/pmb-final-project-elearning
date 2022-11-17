11279e3e716ee3d7a6801e80592d568723fb6285
shirleen-sho <shirleen.sho@gmail.com>
2022-11-17 13:43:13 +0700

    shirleen - update content Raport

--- pages/raport/index.js
+++ pages/raport/index.js
@@ -1,23 +1,48 @@
 import React from "react";
 import Layout from "../../Components/Layout";
+import Selects from "../../Components/Selects";
 import TableMerge from "../../Components/Table/TableMerge";
 import { useAppContext } from "../../Hooks/useAppContext";
 
 const Raport = () => {
   const { dummy } = useAppContext();
-  const { dataDummyTrial1, dataDummyTrial2, dataDummyTrial3, dataDummyTrial4 } =
-    dummy;
+  const { dataDummyTrial2 } = dummy;
+
+  const pilihan_dummy = [
+    { label: "A", value: "a" },
+    { label: "B", value: "b" },
+    { label: "C", value: "c" },
+  ];
   return (
     <Layout>
-      <div className="flex flex-col gap-5">
-        <span>Tabel Trial 1</span>
-        <TableMerge data={dataDummyTrial1} />
-        <span>Tabel Trial 2</span>
-        <TableMerge data={dataDummyTrial2} />
-        <span>Tabel Trial 3</span>
-        <TableMerge data={dataDummyTrial3} />
-        <span>Tabel Trial 4</span>
-        <TableMerge data={dataDummyTrial4} />
+      <div className="w-full h-full flex flex-col gap-5">
+        {/* filter */}
+        <div className="flex flex-row items-center gap-5 my-2 w-full">
+          <div className="flex flex-col gap-2 w-1/3">
+            <span className="font-medium text-xs">Tahun Akademik</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+          <div className="flex flex-col gap-2 w-1/3">
+            <span className="font-medium text-xs">Jenis Ujian</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+          <div className="flex flex-col gap-2 w-1/6">
+            <span className="font-medium text-xs">Tingkatan</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+          <div className="flex flex-col gap-2 w-1/6">
+            <span className="font-medium text-xs">Kelas</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+        </div>
+        <TableMerge
+          data={dataDummyTrial2}
+          fieldSearch
+          numbering
+          pagination
+          actionDetail
+          tableTitle="Daftar Raport Siswa"
+        />
       </div>
     </Layout>
   );

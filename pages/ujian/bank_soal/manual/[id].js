4a53c6157319fb236d4ac84b9c31a9346e930f83
shirleen-sho <shirleen.sho@gmail.com>
2022-11-17 15:54:02 +0700

    shirleen - update content BankSoal Input

--- pages/ujian/bank_soal/manual/[id].js
+++ pages/ujian/bank_soal/manual/[id].js
@@ -26,51 +26,120 @@ const InputManualBankSoal = () => {
   return (
     <Layout>
       <Tabs list={tabList} pathName="/ujian/bank_soal">
-        <div className="flex flex-col gap-8 border border-gray-200 bg-white rounded-xl p-6 shadow-md w-full px-6 py-8 mt-2">
-          {/* judul */}
-          <div className="flex flex-row justify-between pb-7 border-b border-gray-200">
-            <div className="text-sm font-semibold">Soal *nama bank soal*</div>
+        <div className="flex flex-col gap-5 w-full">
+          {/* === CARD SOAL === */}
+          <div className="flex flex-col gap-8 border border-gray-200 bg-white rounded-xl p-6 shadow-md w-full px-6 py-8 mt-2">
+            {/* judul */}
+            <div className="flex flex-row justify-between pb-7 border-b border-gray-200">
+              <div className="text-sm font-semibold">Soal *nama bank soal*</div>
+            </div>
+            <div className="flex flex-row gap-x-10">
+              {/* Mata Pelajaran */}
+              <div className="w-3/12">
+                <FormItem
+                  label="Pilih Opsi"
+                  labelType="label-sm"
+                  formDirection="col"
+                >
+                  <Selects
+                    list={pilihan_dummy}
+                    size="w-full"
+                    description="Pilih opsi"
+                  />
+                </FormItem>
+              </div>
+              {/* Gambar */}
+              <div className="w-4/12">
+                <FormItem
+                  label="Gambar 1"
+                  labelType="label-sm"
+                  formDirection="col"
+                >
+                  <Uploader />
+                </FormItem>
+              </div>
+              {/* + */}
+              <div className="w-1/12 flex justify-end items-end">
+                <Button handleClick={(e) => e.preventDefault()} type="light">
+                  +
+                </Button>
+              </div>
+              {/* Keterangan */}
+              <div className="w-4/12">
+                <FormItem
+                  label="Keterangan Gambar"
+                  labelType="label-sm"
+                  formDirection="col"
+                >
+                  <InputFields
+                    type="text"
+                    placeholder="Tulis keterangan gambar"
+                    size="w-full"
+                  />
+                </FormItem>
+              </div>
+            </div>
           </div>
-          <div className="grid grid-cols-8 gap-x-10">
-            {/* Mata Pelajaran */}
-            <div className="col-start-1 col-span-2 w-full">
-              <FormItem
-                label="Pilih Opsi"
-                labelType="label-sm"
-                formDirection="col"
-              >
-                <Selects
-                  list={pilihan_dummy}
-                  size="w-full"
-                  description="Pilih opsi"
-                />
-              </FormItem>
+          {/* === CARD PILIHAN A === */}
+          <div className="flex flex-col gap-8 border border-gray-200 bg-white rounded-xl p-6 shadow-md w-full px-6 py-8 mt-2">
+            {/* judul */}
+            <div className="flex flex-row justify-between pb-7 border-b border-gray-200">
+              <div className="text-sm font-semibold">Pilihan A</div>
+              <div className="flex flex-row items-center gap-5">
+                <div className="text-sm font-medium">Kunci </div>
+                <input type="checkbox" />
+              </div>
             </div>
-            {/* Gambar */}
-            <div className="col-start-3 col-span-3 bg-yellow-100">
-              <FormItem
-                label="Gambar 1"
-                labelType="label-sm"
-                formDirection="col"
-              >
-                <Uploader />
-              </FormItem>
-            </div>
-            {/* Keterangan */}
-            <div className="col-start-6 col-span-3 bg-yellow-100">
-              <FormItem
-                label="Keterangan Gambar"
-                labelType="label-sm"
-                formDirection="col"
-              >
-                <InputFields
-                  type="text"
-                  placeholder="Tulis keterangan gambar"
-                  size="w-full"
-                />
-              </FormItem>
+            <div className="flex flex-row gap-x-10">
+              {/* Mata Pelajaran */}
+              <div className="w-3/12">
+                <FormItem
+                  label="Pilih Opsi"
+                  labelType="label-sm"
+                  formDirection="col"
+                >
+                  <Selects
+                    list={pilihan_dummy}
+                    size="w-full"
+                    description="Pilih opsi"
+                  />
+                </FormItem>
+              </div>
+              {/* Gambar */}
+              <div className="w-4/12">
+                <FormItem
+                  label="Gambar 1"
+                  labelType="label-sm"
+                  formDirection="col"
+                >
+                  <Uploader />
+                </FormItem>
+              </div>
+              {/* + */}
+              <div className="w-1/12 flex justify-end items-end">
+                <Button handleClick={(e) => e.preventDefault()} type="light">
+                  +
+                </Button>
+              </div>
+              {/* Keterangan */}
+              <div className="w-4/12">
+                <FormItem
+                  label="Keterangan Gambar"
+                  labelType="label-sm"
+                  formDirection="col"
+                >
+                  <InputFields
+                    type="text"
+                    placeholder="Tulis keterangan gambar"
+                    size="w-full"
+                  />
+                </FormItem>
+              </div>
             </div>
           </div>
+          {/* === CARD PILIHAN B === */}
+          {/* === CARD PILIHAN C === */}
+          {/* === CARD PILIHAN D === */}
         </div>
       </Tabs>
     </Layout>
--- pages/ujian/bank_soal/upload/[id].js
+++ pages/ujian/bank_soal/upload/[id].js
@@ -26,10 +26,79 @@ const InputUploadBankSoal = () => {
   return (
     <Layout>
       <Tabs list={tabList} pathName="/ujian/bank_soal">
-        <div className="flex flex-col gap-8 border border-gray-200 bg-white rounded-xl p-6 shadow-md w-full px-6 py-8 mt-2">
-          {/* judul */}
-          <div className="flex flex-row justify-between pb-7 border-b border-gray-200">
-            <div className="text-sm font-semibold">Menu Upload Soal</div>
+        <div className="grid grid-cols-2 gap-8 w-full">
+          {/* === CARD MENU UPLOAD === */}
+          <div className="flex flex-col gap-5 border border-gray-200 bg-white rounded-xl p-6 shadow-md w-full h-fit px-6 py-8 mt-2">
+            {/* judul */}
+            <div className="flex flex-row justify-between pb-7 border-b border-gray-200">
+              <div className="text-sm font-semibold">Menu Upload Soal</div>
+            </div>
+            {/* Import Soal Excel */}
+            <div className="flex flex-col gap-1">
+              <div className="text-sm font-medium">Import Soal Excel</div>
+              <div className="flex flex-row divide-x divide-black">
+                <div className="text-xs pr-4">Berhasil : 50</div>
+                <div className="text-xs pl-2 pr-4">Gagal : 0</div>
+                <div className="text-xs pl-2 pr-4">Dari : 50</div>
+              </div>
+            </div>
+            {/* Nama Soal & Import */}
+            <div className="flex flex-row justify-between items-center gap-5">
+              <InputFields type="text" size="w-full" disabled value={id} />
+              <Button type="primary">Import</Button>
+            </div>
+            {/* Pilih File */}
+            <div className="w-full">
+              <FormItem
+                label="Pilih File"
+                labelType="label-sm"
+                formDirection="col"
+              >
+                <Uploader />
+              </FormItem>
+            </div>
+            {/* Notes */}
+            <div className="text-xs">
+              Sebelum Upload pastikan file yang di-upload sudah dalam bentuk
+              Ms.Excel Workbook (.xls) dan format penulisan harus sesuai dengan
+              templete yang sudah ditentukan.
+            </div>
+            {/* Template */}
+            <div className="text-xs">
+              <button className="text-green-500 font-medium">
+                Download Template/Panduan
+              </button>
+            </div>
+          </div>
+
+          {/* === CARD FILE PENDUKUNG === */}
+          <div className="flex flex-col gap-5 border border-gray-200 bg-white rounded-xl p-6 shadow-md w-full h-fit px-6 py-8 mt-2">
+            {/* judul */}
+            <div className="flex flex-row justify-between pb-7 border-b border-gray-200">
+              <div className="text-sm font-semibold">
+                File Pendukung *nama soal*
+              </div>
+            </div>
+            {/* Pilih File */}
+            <div className="w-full">
+              <FormItem
+                label="Pilih File"
+                labelType="label-sm"
+                formDirection="col"
+              >
+                <Uploader />
+              </FormItem>
+            </div>
+            {/* Notes */}
+            <div className="text-xs">
+              Upload file pendukung soal seperti gambar/audio dan pastikan
+              penulisan nama file yang di-upload sesuai dengan nama file
+              Ms.Excel Workbook (.xls) yang telah di Upload.
+            </div>
+            {/* Import */}
+            <div className="flex flex-row">
+              <Button type="primary">Import</Button>
+            </div>
           </div>
         </div>
       </Tabs>

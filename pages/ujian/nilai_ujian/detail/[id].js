83559f1908334bde1ae6d6d49b191cbae9819f79
shirleen-sho <shirleen.sho@gmail.com>
2022-11-17 08:05:47 +0700

    shirleen - update content nilai ujian

--- pages/ujian/nilai_ujian/detail/[id].js
+++ pages/ujian/nilai_ujian/detail/[id].js
@@ -1,13 +1,61 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../../../Components/Layout";
+import FormItem from "../../../../Components/FormItem";
+import InputFields from "../../../../Components/InputFields";
+import Button from "../../../../Components/Buttons";
 
 const DetailNilaiUjian = () => {
   const router = useRouter();
   const { id } = router.query;
   return (
     <Layout>
-      <div>Detail NilaiUjian {id}</div>
+      <div>
+        <FormItem label="View Nilai Ujian details here!" labelType="banner" />
+        <div className="flex flex-col py-5 gap-5">
+          {/* KODE NILAI UJIAN */}
+          <FormItem
+            label="Kode Nilai Ujian"
+            labelType="label-sm"
+            labelWidth="w-1/4"
+          >
+            <InputFields type="text" size="w-full" disabled />
+          </FormItem>
+
+          {/* Nama Siswa */}
+          <FormItem label="Nama Siswa" labelType="label-sm" labelWidth="w-1/4">
+            <InputFields type="text" size="w-full" disabled />
+          </FormItem>
+
+          {/* Mata Pelajaran */}
+          <FormItem
+            label="Mata Pelajaran"
+            labelType="label-sm"
+            labelWidth="w-1/4"
+          >
+            <InputFields type="text" size="w-full" disabled />
+          </FormItem>
+
+          {/* Tanggal/Waktu Ujian */}
+          <FormItem
+            label="Tanggal/Waktu Ujian"
+            labelType="label-sm"
+            labelWidth="w-1/4"
+          >
+            <InputFields type="text" size="w-full" disabled />
+          </FormItem>
+
+          {/* Nilai */}
+          <FormItem label="Nilai" labelType="label-sm" labelWidth="w-1/4">
+            <InputFields type="text" size="w-full" disabled />
+          </FormItem>
+        </div>
+        <div className="flex flex-row justify-end gap-5">
+          <Button type="light" link="/ujian/nilai_ujian">
+            Back
+          </Button>
+        </div>
+      </div>
     </Layout>
   );
 };
--- pages/ujian/nilai_ujian/index.js
+++ pages/ujian/nilai_ujian/index.js
@@ -1,10 +1,46 @@
 import React from "react";
 import Layout from "../../../Components/Layout";
+import Selects from "../../../Components/Selects";
+import TableBasic from "../../../Components/Table/TableBasic";
+import { useAppContext } from "../../../Hooks/useAppContext";
 
 const NilaiUjian = () => {
+  const { dummy } = useAppContext();
+  const { list_tingkatan } = dummy;
+  const pilihan_dummy = [
+    { label: "A", value: "a" },
+    { label: "B", value: "b" },
+    { label: "C", value: "c" },
+  ];
   return (
     <Layout>
-      <div></div>
+      <div className="w-full h-full flex flex-col gap-5">
+        {/* filter */}
+        <div className="flex flex-row items-center gap-5 my-2 w-full">
+          <div className="flex flex-col gap-2 w-1/3">
+            <span className="font-medium text-xs">Tahun Akademik</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+          <div className="flex flex-col gap-2 w-1/3">
+            <span className="font-medium text-xs">Jenis Ujian</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+          <div className="flex flex-col gap-2 w-1/6">
+            <span className="font-medium text-xs">Tingkatan</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+          <div className="flex flex-col gap-2 w-1/6">
+            <span className="font-medium text-xs">Kelas</span>
+            <Selects list={pilihan_dummy} description="Pilih" size="w-full" />
+          </div>
+        </div>
+        <TableBasic
+          data={list_tingkatan}
+          tableTitle="Daftar Penilaian"
+          actionDetail
+          fieldSearch
+        />
+      </div>
     </Layout>
   );
 };

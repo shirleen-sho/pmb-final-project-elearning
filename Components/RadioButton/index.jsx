b412508670a08f42c59d1acfb4d79ac270197f57
VenziUno <pendicai3@gmail.com>
2022-11-18 09:23:39 +0700

    main - pendi

--- Components/RadioButton/index.jsx
+++ Components/RadioButton/index.jsx
@@ -6,14 +6,12 @@ const RadioButton = () => {
       <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
         <input
           type="checkbox"
-          name="toggle"
-          id="toggle"
           className="toggle-checkbox absolute block w-6 h-6 rounded-full border-gray-400 bg-white border-2 appearance-none cursor-pointer"
         />
-        <label
+        <div
           for="toggle"
           className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-400 cursor-pointer"
-        ></label>
+        ></div>
       </div>
     </>
   );
--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/dashboard",
+        destination: "/login",
         permanent: true,
       },
     ];

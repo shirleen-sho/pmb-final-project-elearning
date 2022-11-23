b5974291165dfa75c1d40ce36ff68761bfa5469b
VenziUno <pendicai3@gmail.com>
2022-11-23 16:22:28 +0700

    pendi - invoice

--- pages/epayment/pembayaran/invoice/index.js
+++ pages/epayment/pembayaran/invoice/index.js
@@ -12,9 +12,25 @@ export default function Invoice() {
     <>
         <div ref={componentRef} className='h-screen w-full'>
             <div>
-                <div className=''>
-                    pendi
+                <div className='flex justify-around items-center'>
+                    <div className='w-64 h-64 bg-primary-100 flex justify-end'>
+                        <div className='flex justify-center '>
+                            <div className='flex justify-end text-2xl '>Invoice</div>
+                        </div>
+                    </div>
+                    <div>No Transaksi #AS1112 - Agustus 08, 2022</div>
                 </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div></div>
+                <div></div>
             </div>
         </div>
         <button onClick={handlePrint}>Print this out</button>
